7cef692a1f561cb43e7af681b83f1569d1ac912b
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2025-03-17 15:47:18 +0700

    initialize MovieMunch project

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>Navbar</div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.js
+import {heroui} from "@heroui/react";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ["var(--font-oswald)"],
+        body: ["var(--font-poppins)"],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [heroui()]
+}
